18621f6c58e30e63b720d417439c63951e8bbad1
angelizu <110600804+angelizu@users.noreply.github.com>
2022-12-07 16:52:57 +0000

    added data source

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -1,5 +1,40 @@
 package bank;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DataSource {
-  
+  public static Connection connect() {
+    String db_file = "jdbc:sqlite:resources/bank.db";
+    Connection connection = null;
+    
+    try {
+      connection = DriverManager.getConnection(db_file);
+      System.out.println("We're connected");
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    
+    return connection;
+  }
+
+public static Customer getCustomer(String username) {
+  String sql = "select * from customers where username = ?";
+  try(Connection connection = connect();
+      PreparedStatement statement = connection.prepareStatement(sql)) {
+        statement.setString(1, username);
+        try(statement.executeQuery();
+  } catch(SQLException e) {
+    e.printStackTrace();
+  }
+}
+
+
+  public static void main(String[] args) {
+    connect();
+  }
+
 }
