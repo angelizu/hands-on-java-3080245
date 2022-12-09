32c5ab1de3291d5c7d246b74cc720e5fea1048f9
angelizu <110600804+angelizu@users.noreply.github.com>
2022-12-09 15:29:34 +0000

    added code to menu

--- src/main/java/bank/Menu.java
+++ src/main/java/bank/Menu.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.util.Scanner;
 
+import javax.security.auth.login.LoginException;
+
 public class Menu {
   
   private Scanner scanner;
@@ -11,10 +13,58 @@ public class Menu {
     Menu menu = new Menu();
     menu.scanner = new Scanner(System.in);
     
-
-
+    Customer customer = menu.authenticateUser();
+    if(customer !=null) {
+      Account account = DataSource.getAccount(customer.getAccountId());
+      menu.showMenu(customer, account);
+    }
+    
 
     menu.scanner.close();
+  }
+
+  private Customer authenticateUser() {
+    System.out.println("Please enter your username:");
+    String username = scanner.next();
+
+    System.out.println("Please enter your password:");
+    String password = scanner.next();
+
+    Customer customer = null;
+
+    try {
+      customer = Authenticator.login(username, password);
+    } catch (LoginException e) {
+      System.out.println("There was an error " + e.getMessage());
+    }
+
+    return customer;
+  }
+
+  private void showMenu(Customer customer, Account account) {
+    int selection = 0;
+
+    while (selection != 4 && customer.isAuthenticated()) {
+      System.out.println("===============================================");
+      System.out.println("Please select one of the following options: ");
+      System.out.println("1: Deposit");
+      System.out.println("2: Withdraw");
+      System.out.println("3: Check balance");
+      System.out.println("4: Exit");
+      System.out.println("===============================================");
+      
+      selection = scanner.nextInt();
+      double amount = 0;
+
+      switch(selection) {
+        case 1:
+          System.out.println("How much would you like to deposit");
+          amount = scanner.nextDouble();
+          account.deposit(amount);
+          break;
+
+      }
+    }
   }
 
 }
